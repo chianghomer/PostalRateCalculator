a287c89fe5737679f022fddbcfcb36912516916d
chianghomer <chianghomer@gmail.com>
2016-02-19 12:15:47 -0500

    second commit

---
+++ app/src/main/java/ecse428/mcgill/ca/postalratecalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package ecse428.mcgill.ca.postalratecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
